295e380ec3ea103a5b5f8ecbc03c84db7dbd4fa6
Tim Bartsch <tibartschm@googlemail.com>
2012-10-30 19:26:27 +0100

    A lot of changes. Ending processes works now.

--- node.rb
+++ node.rb
@@ -5,34 +5,54 @@ require 'json'
 
 abort("Usage: #{__FILE__} ID [NEIGHBOR_IDs]") unless ARGV.size() > 0
 
-id = ARGV[0]
+HOST = 'localhost'
+NAME = ARGV[0]
+
 neighbors = ARGV.slice(1, ARGV.size() -1)
 
-puts "ID: #{id}"
-puts "Neighbors: #{neighbors.join(" ")}"
+puts "ID: #{NAME} Neighbors: #{neighbors.join(" ")}"
 
-server = TCPServer.new id
+server = TCPServer.new(NAME) 
 
-def send_to(neighbors, msg)
-	host = 'localhost'
-	neighbors.each {|neighbor|
-		puts "#{neighbor}"
-		socket = TCPSocket.open(host, neighbor)
+def send(destination, action)
+	begin
+		socket = TCPSocket.open(HOST, destination)
+		msgHash = {'sender'=>NAME, 'destination'=>destination, 'action'=>action}
+		msg = JSON.generate(msgHash)
 		socket.puts("#{msg}\r\n")
-		puts "#{Time.now}: #{msg}"
-		socket.close
-	}
+		puts "#{Time.now.strftime("%H:%M:%S")} | #{NAME} >>> #{msg}"
+	rescue
+		puts "#{Time.now.strftime("%H:%M:%S")} | #{NAME} ::: #{$!}"
+	ensure
+		if(socket != nil)
+			socket.close
+		end
+	end
+end
+
+def isForMe(msg)
+	return msg['destination'] == NAME
 end
 
 loop do
 	Thread.start(server.accept) do |socket|
 		begin
-			requestMsg = socket.gets
-			puts "#{Time.now}: #{requestMsg}"
-			responseMsg = JSON.generate({"responseMsg"=>'Hello.', "senderId"=> id})
-			send_to(neighbors, responseMsg)
-		rescue
-			puts "#{$!}"
+			requestString = socket.gets
+			puts "#{Time.now.strftime("%H:%M:%S")} | #{NAME} <<< #{requestString}"
+			requestHash = JSON.parse(requestString)
+			if(isForMe(requestHash))
+				neighbors.each {|neighbor|
+					if(neighbor != requestHash['sender'])
+						send(neighbor, "quit")
+					end
+				}
+				if(requestHash['action'] == 'quit')
+						puts "#{Time.now.strftime("%H:%M:%S")} | #{NAME} ::: Shutting down."
+						abort()
+				end
+			end
+		rescue StandardError => e
+			puts "#{Time.now.strftime("%H:%M:%S")} | #{NAME} eee #{e.backtrace}"
 		ensure
 			socket.close
 		end
