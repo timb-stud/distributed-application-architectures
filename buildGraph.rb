be07af24f574abc3695ef4af2963c85c3c332b46
Tim Bartsch <tibartschm@googlemail.com>
2012-11-07 20:47:18 +0100

    buildGraph.rb generates a graphviz output for a given amount of nodes.

---
+++ buildGraph.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+abort("Usage: #{__FILE__} NODES") unless ARGV.size() == 1
+
+NODES = Integer(ARGV[0])
+PORT = 2000
+
+nodesHash = Hash.new
+
+def randId()
+	return PORT + rand(NODES)
+end
+
+while nodesHash.length < NODES
+	nodeLeft = randId()
+	nodeRight = randId()
+	#puts "#{nodeLeft} #{nodeRight} #{(nodesHash[nodeRight] != nil)} #{!(nodesHash[nodeRight] && nodesHash[nodeRight].include?(nodeLeft))}"
+	if(nodeLeft != nodeRight && !(nodesHash[nodeRight] && nodesHash[nodeRight].include?(nodeLeft)))
+		if(nodesHash[nodeLeft])
+			unless(nodesHash[nodeLeft].include?(nodeRight))
+				nodesHash[nodeLeft] << nodeRight 
+			end
+		else
+			nodesHash[nodeLeft] = [nodeRight]
+		end
+	end
+end
+
+
+puts "graph Nodes {"
+nodesHash.each {|key, value|
+	value.each{|n|
+		puts "	#{key} -- #{n}"
+	}
+}
+puts "}"
